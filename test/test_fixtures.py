dd93cbe6efed2ebd27e94e4dcfc29adda4b56dba
Ahmed Adel <github@aadel.io>
2021-01-15 13:11:28 -0800

    Renaming test function

--- test/test_fixtures.py
+++ test/test_fixtures.py
@@ -5,6 +5,6 @@ BASE_URL = 'http://solr:8983/solr'
 f = SalesFixture(BASE_URL)
 
 class TestData():
-    def test_index_sales_data(self):
+    def test_indexing_sales_data(self):
         f.truncate_collection()
         f.index()
