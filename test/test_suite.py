086a302873d7a8bd9c4fa5baa6ea8ddd10605284
Ahmed Adel <github@aadel.io>
2021-01-17 08:00:38 -0800

    Renaming Solr base URL variable

--- test/test_suite.py
+++ test/test_suite.py
@@ -2,10 +2,10 @@ from .fixtures.fixtures import SalesFixture
 from .test_db_creation import DBCreationTest
 
 class TestSuite:
-    BASE_URL = 'http://solr:8983/solr'
+    SOLR_BASE_URL = 'http://solr:8983/solr'
     
     def test_indexing_data(self):
-        f = SalesFixture(TestSuite.BASE_URL)
+        f = SalesFixture(TestSuite.SOLR_BASE_URL)
         f.truncate_collection()
         f.index()
 
