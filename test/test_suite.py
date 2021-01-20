465802c32820a6a039c21f80bcec3539cea2f530
Ahmed Adel <github@aadel.io>
2021-01-19 23:44:30 -0800

    Testing Solr compiler

--- test/test_suite.py
+++ test/test_suite.py
@@ -1,3 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy import select, and_
+from sqlalchemy import Table, MetaData
+
 from .fixtures.fixtures import SalesFixture
 from .test_db_creation import DBCreationTest
 
@@ -11,3 +15,15 @@ class TestSuite:
 
     def test_db_creation(self):
         DBCreationTest()
+
+    def test_solr_compiler(self):
+        metadata = MetaData()
+        engine = create_engine('solr://solr:8983/solr/sales_test_', echo=True)
+        t = Table('sales_test_', metadata, autoload=True, autoload_with=engine)
+        qry = (select([t.columns['CITY_s']]).select_from(t)).limit(100)             # pylint: disable=unsubscriptable-object
+        qry = qry.where(and_(t.columns['ORDERDATE_dt'] >= '2017-05-10 00:00:00',    # pylint: disable=unsubscriptable-object
+            t.columns['ORDERDATE_dt'] <= '2017-05-20 00:00:00'))                    # pylint: disable=unsubscriptable-object
+
+        result = engine.execute(qry)
+
+        assert result.rowcount == 18
