f9a2eaecf31674e39c5e46166b69acc3fef55616
Ahmed Adel <github@aadel.io>
2021-04-13 04:52:56 -0700

    Instantiating type map types

--- sqlalchemy_solr/base.py
+++ sqlalchemy_solr/base.py
@@ -36,21 +36,21 @@ from sqlalchemy_solr.solrdbapi.array import ARRAY
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.ERROR)
 
 _type_map = {
-    "binary": types.LargeBinary,
-    "boolean": types.Boolean,
-    "pdate": types.DateTime,
-    "pint": types.Integer,
-    "plong": types.BigInteger,
-    "pfloat": types.Float,
-    "pdouble": types.REAL,
-    "string": types.String,
-    "text_general": types.Text,
-    "booleans": ARRAY(types.BOOLEAN),
-    "pints": ARRAY(types.Integer),
-    "plongs": ARRAY(types.BigInteger),
-    "pfloats": ARRAY(types.Float),
-    "pdoubles": ARRAY(types.REAL),
-    "strings": ARRAY(types.String),
+    "binary": types.LargeBinary(),
+    "boolean": types.Boolean(),
+    "pdate": types.DateTime(),
+    "pint": types.Integer(),
+    "plong": types.BigInteger(),
+    "pfloat": types.Float(),
+    "pdouble": types.REAL(),
+    "string": types.VARCHAR(),
+    "text_general": types.Text(),
+    "booleans": ARRAY(types.BOOLEAN()),
+    "pints": ARRAY(types.Integer()),
+    "plongs": ARRAY(types.BigInteger()),
+    "pfloats": ARRAY(types.Float()),
+    "pdoubles": ARRAY(types.REAL()),
+    "strings": ARRAY(types.VARCHAR()),
 }
 
 
