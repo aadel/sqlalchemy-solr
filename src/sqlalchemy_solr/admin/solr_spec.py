e850336026ef99d43722a05d61519f415f73dcdc
Ahmed Adel <github@aadel.io>
2024-02-29 20:24:28 +0000

    Creating SolrSpec class

---
+++ src/sqlalchemy_solr/admin/solr_spec.py
@@ -0,0 +1,17 @@
+from requests import Session
+
+class SolrSpec:
+
+    _spec = None
+
+    def __init__(self, solr_base_url):
+        session = Session()
+        sys_info_response = session.get(solr_base_url + "/admin/info/system")
+        spec_version = sys_info_response.json()["lucene"]["solr-spec-version"]
+        self._spec = list(map(int, spec_version.split('.')))
+
+    def __str__(self) -> str:
+        return '.'.join(list(map(str, self._spec)))
+
+    def spec(self):
+        return self._spec
