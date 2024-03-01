28ae143fa9466f795bdaeb7d16ac3396acc66651
Ahmed Adel <github@aadel.io>
2024-03-01 17:12:34 +0000

    Creating http fixture

--- tests/test_db_creation.py
+++ tests/test_db_creation.py
@@ -1,13 +1,9 @@
-from requests import Session
-
 from .steps import TestSteps
 
 
 class TestDBCreation:
     # pylint: disable=too-few-public-methods
-    def test_db_creation(self, settings):
+    def test_db_creation(self, settings, http):
         test_steps = TestSteps(settings)
-        session = Session()
-        headers = test_steps.login(session)
-        db_creation_response = test_steps.create_database(session, headers)
+        db_creation_response = test_steps.create_database(http['session'], http['headers'])
         assert db_creation_response.status_code == 201
