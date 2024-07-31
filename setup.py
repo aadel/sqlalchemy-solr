0d89f2ea5aa163b491900a1d4b42a2c0ee5bf910
Ahmed Adel <github@aadel.io>
2024-07-31 20:55:48 +0300

    fix: Adding missing comma

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(readme, encoding="utf-8") as f:
 setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="Ahmed Adel"
+    author="Ahmed Adel",
     tests_require=["pysolr", "pytest >= 6.2.1"],
     test_suite="nose.collector",
     zip_safe=False,
